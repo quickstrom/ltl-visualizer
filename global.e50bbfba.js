529ec84b838ab371c7aae12667bd390279031c52
owickstrom <oskar@wickstrom.tech>
2021-05-26 20:36:07 +0200

    Update documentation

--- global.e50bbfba.js
+++ global.e50bbfba.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32849" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39355" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
